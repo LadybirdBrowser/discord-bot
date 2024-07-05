8cc4f2cd4ae8c443d009e5d6baabe37aa1569f8d
versecafe <147033096+versecafe@users.noreply.github.com>
2024-07-05 01:43:56 -0400

    formatter config

---
+++ prettier.config.mjs
@@ -0,0 +1,15 @@
+const config = {
+  arrowParens: "avoid",
+  bracketSpacing: true,
+  endOfLine: "lf",
+  insertPragma: false,
+  printWidth: 100,
+  quoteProps: "as-needed",
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "es5",
+  useTabs: false,
+};
+
+export default config;
